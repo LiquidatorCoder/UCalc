0be4da092dbf56bb82ebbd3ca38a1ccfae59d434
LiquidatorCoder <maurya.abhay30@gmail.com>
2021-11-30 13:30:36 +0530

    add backspace & number onclick actions

--- app/src/main/java/com/abhay/ucalc/MainActivity.kt
+++ app/src/main/java/com/abhay/ucalc/MainActivity.kt
@@ -2,10 +2,34 @@ package com.abhay.ucalc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun numberAction(view: View) {
+        if (view is Button)
+        {
+            workingsTV.append(view.text)
+        }
+    }
+
+    fun operationAction(view: View) {}
+    fun allClearAction(view: View) {
+        workingsTV.text = ""
+        resultsTV.text = ""
+    }
+
+    fun backSpaceAction(view: View) {
+        val length = workingsTV.length()
+        if (length > 0)
+            workingsTV.text = workingsTV.text.subSequence(0, length - 1)
+    }
+
+    fun equalsAction(view: View) {}
 }
